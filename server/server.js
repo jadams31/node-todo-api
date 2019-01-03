4d8cb67f4de526012834558f83e58247c197248c
Adams, Joshua <jadams@netsuite.com>
2019-01-03 17:47:27 -0600

    added login

--- server/server.js
+++ server/server.js
@@ -34,6 +34,18 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then, ((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 //Todo routes
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
