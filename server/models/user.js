e5bdac52fa323f7d9fc4276b6b282e8d92ee244f
Adams, Joshua <jadams@netsuite.com>
2019-01-03 16:14:50 -0600

    added authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,24 @@ UserSchema.methods.generateAuthToken = function () { //arrow functions do not bi
   });
 }
 
+//Static methods defined only on class not instance
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
